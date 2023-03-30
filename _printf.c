6f085a301180ac97d6662e2151cee5629d3f27ec
cohort20 <sophia.jiyu@gmail.com>
2023-03-30 04:59:21 -0700

    update code to handle single %

--- _printf.c
+++ _printf.c
@@ -40,7 +40,7 @@ int _printf(const char *format, ...)
 	};
 
 	va_start(args, format);
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	while (format[i] != '\0')
 	{
